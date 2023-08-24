1c87e771a9af4ebc2bc4b99b5463342ac1a3c80d
gnaohlord1012 <hoang.dinh10122001@gmail.com>
2023-08-24 16:41:15 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\CompanyController;
+ 
+Route::resource('companies', CompanyController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
